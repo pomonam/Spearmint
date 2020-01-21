2247460b7602c1a2c467f7b6cef245aed19f088e
Juhan Bae <pomonam15@gmail.com>
2020-01-20 21:36:09 -0500

    minor

--- spearmint/models/gp_classifier.py
+++ spearmint/models/gp_classifier.py
@@ -191,7 +191,6 @@ import scipy.linalg      as spla
 import scipy.optimize    as spo
 import scipy.io          as sio
 import scipy.stats       as sps
-import scipy.weave
 
 
 from .gp                                     import GP
